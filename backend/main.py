acbf451eb639247add23f7645b59314525c61e7a
Muni Kumar <116535095+Munikumar09@users.noreply.github.com>
2025-02-09 16:51:29 +0530

    Add Rate Limiter To Verification Code Sending Endpoints (#66)
    ## Description
    -> Add rate limiters to the verification sending endpoints
    -> Add tests to ensure the rate limiter functionality
    
    ## Summary
    
    - **New Features**
    	- Added rate limiting on authentication endpoints for sending verification and reset codes, ensuring secure and consistent interactions.
    	- Enhanced request validations improve the reliability of the authentication process, contributing to a smoother user experience.
    	- Introduced a new step to verify PostgreSQL connection before running tests, improving error handling.
    	- Implemented asynchronous testing capabilities for authentication, enhancing test robustness.
    	- Integrated Redis for rate limiting, improving performance and scalability.
    
    - **Bug Fixes**
    	- Improved error handling for missing email in rate limiting functionality.
    
    - **Chores**
    	- Updated dependencies for testing and added new libraries for rate limiting and Redis integration.

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,59 @@
 # pylint: disable=missing-function-docstring
 
+from pathlib import Path
+
+import redis.asyncio as redis
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi_limiter import FastAPILimiter
 
 from app.data_layer.database.db_connections import postgresql
 from app.routers.authentication import authentication
 from app.routers.nse.derivatives import derivatives
 from app.routers.nse.equity import equity
 from app.routers.smartapi.smartapi import smartapi
+from app.utils.common.logger import get_logger
+from app.utils.fetch_data import get_required_env_var
 from app.utils.startup_utils import create_smartapi_tokens_db
 
+logger = get_logger(Path(__file__).name)
+
+load_dotenv()
+
 app = FastAPI()
+
+
+# Initialize Redis client
+REDIS_HOST = get_required_env_var("REDIS_HOST")
+REDIS_PORT = int(get_required_env_var("REDIS_PORT"))
+REDIS_DB = get_required_env_var("REDIS_DB")
+try:
+    redis_client = redis.Redis(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True
+    )
+    # Test connection
+    redis_client.ping()
+except redis.ConnectionError as e:
+    logger.error("Failed to connect to Redis: %s", str(e))
+    raise
+
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        postgresql.create_db_and_tables()
+        create_smartapi_tokens_db()
+        await FastAPILimiter.init(redis_client)
+    except Exception as e:
+        logger.error("Failed to initialize startup event: %s", str(e))
+        raise
+
 
 app.include_router(derivatives.router)
 app.include_router(equity.router)
 app.include_router(smartapi.router)
 app.include_router(authentication.router)
-
-
-@app.on_event("startup")
-async def startup_event():
-    postgresql.create_db_and_tables()
-    create_smartapi_tokens_db()
 
 
 @app.get("/", response_model=str)
